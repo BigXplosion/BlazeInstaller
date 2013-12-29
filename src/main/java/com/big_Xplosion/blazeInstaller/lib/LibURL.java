ed929c0f59fa87aeb5634a2b52c788fbf4dc91b5
big_Xplosion <bigXplosion44@gmail.com>
2013-12-29 14:38:10 +0100

    More base classes

---
+++ src/main/java/com/big_Xplosion/blazeInstaller/lib/LibURL.java
@@ -0,0 +1,20 @@
+package com.big_Xplosion.blazeInstaller.lib;
+
+public class LibURL
+{
+	public static final String MC_DOWNLOAD_URL = "https://s3.amazonaws.com/Minecraft.Download/";
+
+	public static final String MC_DOWNLOAD_ROOT_VERSION_URL = MC_DOWNLOAD_URL + "versions/";
+	public static final String MC_DOWNLOAD_VERSION_URL = MC_DOWNLOAD_ROOT_VERSION_URL + "{MC_VERSION}/";
+
+	public static final String MC_INDEX_URL = MC_DOWNLOAD_URL + "indexes/";
+
+	public static final String MC_DOWNLOAD_LIB_ROOT_URL = "https://libraries.minecraft.net/";
+
+	public static final String MC_DOWNLOAD_JAR_URL = MC_DOWNLOAD_VERSION_URL + "{MC_VERSION}.jar";
+	public static final String MC_DOWNLOAD_SERVER_JAR_URL = MC_DOWNLOAD_VERSION_URL + "minecraft_server.{MC_VERSION}.jar";
+
+	public static final String MC_ASSET_INDEX_URL = MC_INDEX_URL + "{ASSET_INDEX}.json";
+
+	public static final String MCP_DOWNLOAD_URL = "http://mcp.ocean-labs.de/files/archive/mcp804.zip";
+}
---
+++ src/main/java/com/big_Xplosion/blazeInstaller/util/OS.java
@@ -0,0 +1,60 @@
+package com.big_Xplosion.blazeInstaller.util;
+
+import java.io.File;
+
+public enum OS
+{
+	WINDOWS("windows", "win"), OSX("osx", "mac", "macos"), LINUX("linux", "unix"), SOLARIS("solaris", "sunos"), UNKNOWN("unknown");
+
+	private String osName;
+	private String[] aliases;
+
+	private OS(String osName, String... aliases)
+	{
+		this.osName = osName;
+		this.aliases = aliases;
+	}
+
+	public static OS getCurrentPlatform()
+	{
+		String osName = System.getProperty("os.name").toLowerCase();
+
+		for (OS os : values())
+		{
+			if (osName.contains(os.osName))
+				return os;
+
+			for (String alias : os.aliases)
+				if (osName.contains(alias))
+					return os;
+		}
+
+		return UNKNOWN;
+	}
+
+	public static String getOSVersion()
+	{
+		return System.getProperty("os.version");
+	}
+
+	public static File getMinecraftDirectory()
+	{
+		String homeDir = System.getProperty("user.home");
+
+		switch (getCurrentPlatform())
+		{
+			case WINDOWS :
+				String appData = System.getenv("APPDATA");
+				String folder = appData != null ? appData : homeDir;
+				return new File(folder, ".minecraft/");
+			case OSX :
+				return new File(homeDir, "Library/Application Support/minecraft");
+			case LINUX :
+				return new File(homeDir, ".minecraft/");
+			case SOLARIS :
+				return new File(homeDir, ".minecraft/");
+			default :
+				return new File(homeDir, "minecraft/");
+		}
+	}
+}
