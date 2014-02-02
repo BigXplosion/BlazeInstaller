f378ca9c000efc1d265b3e1bae6ab9ddaa584cd8
big_Xplosion <bigXplosion44@gmail.com>
2014-02-02 14:49:37 +0100

    Beginnings of the clientInstall.

--- src/main/java/com/big_Xplosion/blazeInstaller/action/ClientInstall.java
+++ src/main/java/com/big_Xplosion/blazeInstaller/action/ClientInstall.java
@@ -6,15 +6,27 @@ import java.io.IOException;
 public class ClientInstall implements IInstallerAction
 {
     @Override
-    public boolean install(File file) throws IOException
+    public boolean install(File target) throws IOException
     {
-        return false;
+        File versionRoot = new File(target, "versions");
+        File libRoot = new File(target, "libraries");
+
+        if (!downloadBL(target))
+            return false;
+
+        return true;
     }
 
     @Override
-    public boolean isPathValid(File targetFile)
+    public boolean isPathValid(File target)
     {
-        return true;
+        if (target.exists())
+        {
+            File profiles = new File(target, "launcher_profiles.json");
+            return profiles.exists();
+        }
+
+        return false;
     }
 
     @Override
@@ -30,6 +42,11 @@ public class ClientInstall implements IInstallerAction
             return "The directory is missing a launcher profile. Please run the minecraft launcher first";
         else
             return "There is no minecraft directory set up. Either choose an alternative, or run the minecraft launcher to create one";
+    }
 
+    private boolean downloadBL(File target)
+    {
+        // TODO: implement when we have a proper way of doing it.
+        return true;
     }
 }
